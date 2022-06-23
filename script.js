6888304df7e1a44b13599e8e53a850819c202cee
Ahmed Radwan <Ahmed.Radwan@live.ie>
2022-06-23 11:46:44 +0100

    Adjusting some UI elements

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 'use strict';
 
-// const diceImages = [
-//   'img/1.svg',
-//   'img/2.svg',
-//   'img/3.svg',
-//   'img/4.svg',
-//   'img/5.svg',
-//   'img/6.svg'
-// ]
-
+//Get a Random number
 const randNum = function () {
   return Math.trunc(Math.random() * 6) + 1
 }
 
 //DOM Elements selected
-
 const score0Elem = document.querySelector('.score--0')
 const score1Elem = document.querySelector('.score--1')
 const currentScore0Elem = document.getElementById('current--0')
@@ -28,13 +19,24 @@ const btnKeepElem = document.querySelector('.btn--keep')
 //Global var
 let currentScore = 0;
 let totalScore = 0;
-// let collectScore = 0;
+
 let activePlayer = 0;
 let scoreArray = [0, 0];
 
-// score0Elem.textContent = '0'
-// score1Elem.textContent = '0'
-// diceElem.classList.add('.hidden');
+//When click on new Game btn
+btnNewElem.addEventListener('click', function () {
+  score0Elem.textContent = '0'
+  score1Elem.textContent = '0'
+  currentScore0Elem.textContent = 0
+  currentScore1Elem.textContent = 0
+
+  currentScore = 0;
+  totalScore = 0;
+  activePlayer = 0;
+  scoreArray = [0, 0]
+})
+
+
 
 //When click on roll btn
 btnRollElem.addEventListener('click', function () {
@@ -50,35 +52,25 @@ btnRollElem.addEventListener('click', function () {
     activePlayer = activePlayer === 0 ? 1 : 0;
 
   } else {
-
     currentScore += currentRand;
     totalScore = currentScore;
-
     document.getElementById(`current--${activePlayer}`).textContent = totalScore
-
   }
-
 })
 
 btnKeepElem.addEventListener('click', function () {
 
-  // collectScore[activePlayer] = totalScore
   scoreArray[activePlayer] += totalScore
   currentScore = 0;
 
-  if (collectScore >= 100) {
+  if (scoreArray[activePlayer] >= 100) {
     document.getElementById(`score--${activePlayer}`).textContent = 'Winner';
   }
-
   document.getElementById(`score--${activePlayer}`).textContent = scoreArray[activePlayer]
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   totalScore = 0;
 
-
-
-
   document.querySelector('.player--0').classList.toggle('player--active');
   document.querySelector('.player--1').classList.toggle('player--active');
   activePlayer = activePlayer === 0 ? 1 : 0;
-
 })
