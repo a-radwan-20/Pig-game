e506432fa1b62981fcfa1223fe6c079464ec5ffb
Ahmed Radwan <Ahmed.Radwan@live.ie>
2022-06-23 14:26:04 +0100

    Clean code and formats

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 'use strict';
 
-//Get a Random number
-const randNum = function () {
-  return Math.trunc(Math.random() * 6) + 1
-}
-
 //DOM Elements selected
 const score0Elem = document.querySelector('.score--0')
 const score1Elem = document.querySelector('.score--1')
-const currentScore0Elem = document.getElementById('current--0')
-const currentScore1Elem = document.getElementById('current--1')
+const currentScore0Elem = document.querySelector('.current--0')
+const currentScore1Elem = document.querySelector('.current--1')
 
 const diceElem = document.querySelector('.dice');
 const btnNewElem = document.querySelector('.btn--new')
@@ -23,20 +18,40 @@ let totalScore = 0;
 let activePlayer = 0;
 let scoreArray = [0, 0];
 
-//When click on new Game btn
-btnNewElem.addEventListener('click', function () {
-  score0Elem.textContent = '0'
-  score1Elem.textContent = '0'
+//Get a Random number
+const randNum = function () {
+  return Math.trunc(Math.random() * 6) + 1
+}
+
+//Function to switch a player
+const switchPlayer = function () {
+  document.querySelector('.player--0').classList.toggle('player--active');
+  document.querySelector('.player--1').classList.toggle('player--active');
+  document.getElementById(`current--${activePlayer}`).textContent = 0
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+}
+
+//When New Game Initiated
+const resetAll = function () {
+  diceElem.src = `img/0.svg`
+
+  btnKeepElem.removeAttribute('disabled', 'disabled')
+  btnRollElem.removeAttribute('disabled', 'disabled')
+
+  score0Elem.textContent = 0
+  score1Elem.textContent = 0
   currentScore0Elem.textContent = 0
   currentScore1Elem.textContent = 0
+
+  document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner')
+  document.querySelector(`.player--0`).classList.add('player--active')
 
   currentScore = 0;
   totalScore = 0;
   activePlayer = 0;
   scoreArray = [0, 0]
-})
-
-
+}
 
 //When click on roll btn
 btnRollElem.addEventListener('click', function () {
@@ -44,13 +59,7 @@ btnRollElem.addEventListener('click', function () {
   diceElem.src = `img/${currentRand}.svg`
 
   if (currentRand === 1) {
-
-    document.querySelector('.player--0').classList.toggle('player--active');
-    document.querySelector('.player--1').classList.toggle('player--active');
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-
+    switchPlayer()
   } else {
     currentScore += currentRand;
     totalScore = currentScore;
@@ -62,15 +71,20 @@ btnKeepElem.addEventListener('click', function () {
 
   scoreArray[activePlayer] += totalScore
   currentScore = 0;
-
-  if (scoreArray[activePlayer] >= 100) {
-    document.getElementById(`score--${activePlayer}`).textContent = 'Winner';
-  }
   document.getElementById(`score--${activePlayer}`).textContent = scoreArray[activePlayer]
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  totalScore = 0;
 
-  document.querySelector('.player--0').classList.toggle('player--active');
-  document.querySelector('.player--1').classList.toggle('player--active');
-  activePlayer = activePlayer === 0 ? 1 : 0;
+  if (scoreArray[activePlayer] >= 20) {
+    document.getElementById(`score--${activePlayer}`).textContent = `Winner`
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    btnKeepElem.setAttribute('disabled', 'disabled')
+    btnRollElem.setAttribute('disabled', 'disabled')
+
+  } else {
+    switchPlayer()
+  }
+
 })
+
+//When click on new Game btn
+btnNewElem.addEventListener('click', resetAll)
